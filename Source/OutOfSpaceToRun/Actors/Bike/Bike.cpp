4875ddc7aad99561359b6712fb8f8d38fbbd7e74
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-07-27 13:08:23 +0200

    Moving code to C++ files. Moved Turn and Pivot

---
+++ Source/OutOfSpaceToRun/Actors/Bike/Bike.cpp
@@ -0,0 +1,194 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OutOfSpaceToRun/Actors/Bike/Bike.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+
+// Sets default values
+ABike::ABike()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Defaults
+	Fuel = 100.0f;
+	FuelBurnRate = 1.0f;
+	IsAlive = true;
+	IsBoosting = false;
+	DefaultMovementSpeed = 50.0f;
+	MovementSpeedModifier = 1.0f;
+	PanSpeed = 5.0f;
+
+	MainChassis = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Main Chassis"));
+	MainChassis->AddToRoot();
+	Engine = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Engine"));
+	Engine->SetupAttachment(MainChassis);
+	HandleBars = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HandleBars"));
+	HandleBars->SetupAttachment(MainChassis);
+	FeetSupport = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FeetSupport"));
+	FeetSupport->SetupAttachment(MainChassis);
+	WheelsGuards = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WheelsGuards"));
+	WheelsGuards->SetupAttachment(MainChassis);
+	Wheels = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Wheels"));
+	Wheels->SetupAttachment(MainChassis);
+	Roof = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Roof"));
+	Roof->SetupAttachment(MainChassis);
+
+	//// Setup parameters in editor
+	//SpringArmMinimap = CreateDefaultSubobject<USpringArmComponent>(TEXT("Minimap Spring Arm"));
+	//SpringArmMinimap->SetupAttachment(MainChassis);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Player Camera"));
+	CameraBoom->SetupAttachment(MainChassis);
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+}
+
+// Called when the game starts or when spawned
+void ABike::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+
+}
+
+// Turns vehicle by 90 degrees
+void ABike::Turn(const FInputActionValue& Value)
+{
+	if (!IsAlive)
+		return;
+	
+	TObjectPtr<UPawnMovementComponent> MovementComponent = GetMovementComponent();
+	if (!MovementComponent)
+		return;
+
+	if (!MovementComponent->IsMovingOnGround())
+		return;
+	float Val = Value.Get<float>();
+	auto MainChassisRotation = MainChassis->GetComponentRotation();
+	float OrigYaw = MainChassisRotation.Yaw;
+	float NewYaw = OrigYaw + 90.0f * Val;
+	NewYaw = FMath::Modulo(NewYaw, 360.0f);
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow,FString::Printf(TEXT("Turn: %f"), NewYaw));
+
+	FRotator NewRotation( MainChassisRotation.Pitch, NewYaw, MainChassisRotation.Roll);
+	MainChassis->SetWorldRotation(NewRotation);
+	
+}
+
+void ABike::Pivot(const FInputActionValue& Value)
+{
+	if (!IsAlive)
+		return;
+
+	TObjectPtr<UPawnMovementComponent> MovementComponent = GetMovementComponent();
+	if (!MovementComponent)
+		return;
+
+	if (!MovementComponent->IsMovingOnGround())
+		return;
+
+	float Val = Value.Get<float>();
+
+	auto MainChassisRelativeRotation = MainChassis->GetRelativeRotation();
+
+	float NewPivot = MainChassisRelativeRotation.Yaw + Val * PanSpeed;
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Pivot: %f"), NewPivot));
+
+	FRotator NewRotation(MainChassisRelativeRotation.Pitch, NewPivot, MainChassisRelativeRotation.Roll);
+
+	MainChassis->SetRelativeRotation(NewRotation);
+
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Pivot"));
+}
+
+void ABike::ConstantForwardMovement()
+{
+	TObjectPtr<UPawnMovementComponent> MovementComponent = GetMovementComponent();
+	if (MovementComponent)
+	{
+		bool IsMovingOnGround = MovementComponent->IsMovingOnGround();
+
+		// TODO: Perhaps allow boosting while in air as well..
+		if (IsMovingOnGround)
+		{
+			// assumes that we can boost
+			if (IsBoosting && (Fuel > 0.0f))
+			{
+				MovementSpeedModifier = 1.f;
+				Fuel -= FuelBurnRate;
+				if (GEngine)
+					GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Fuel %f"), Fuel));
+
+			}
+			else
+			{
+				MovementSpeedModifier = 0.5f;
+			}
+		}
+	}
+}
+
+void ABike::StartBoosting()
+{
+	IsBoosting = true;
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Is Boosting"));
+}
+
+void ABike::StopBoosting()
+{
+	IsBoosting = false;
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Not Boosting"));
+}
+
+// Called every frame
+void ABike::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//ConstantForwardMovement();
+
+}
+
+// Called to bind functionality to input
+void ABike::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
+
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+		// Boosting 
+		EnhancedInputComponent->BindAction(BoostAction, ETriggerEvent::Started, this, &ABike::StartBoosting);
+		EnhancedInputComponent->BindAction(BoostAction, ETriggerEvent::Completed, this, &ABike::StopBoosting);
+
+		// Turning by 90 degrees
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Started, this, &ABike::Turn);
+
+		// Pivoting, this gives ability to swerve, not just simply turn by 90 degrees
+		EnhancedInputComponent->BindAction(PivotAction, ETriggerEvent::Triggered, this, &ABike::Pivot);
+	}
+}
+
---
+++ Source/OutOfSpaceToRun/Actors/Bike/Bike.h
@@ -0,0 +1,133 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Bike.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+class UStaticMeshComponent;
+class USkeletalMeshComponent;
+class UArrowComponent;
+struct FInputActionValue;
+
+UCLASS()
+class OUTOFSPACETORUN_API ABike : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArmMinimap;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* TurnAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* PivotAction;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* BoostAction;
+
+protected:
+
+	// This one is taken from BP I've used previously, don't recall why
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* CharacterMesh;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MainChassis;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Engine;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* HandleBars;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* FeetSupport;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* WheelsGuards;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Wheels;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Roof;
+
+	UPROPERTY(EditAnywhere)
+	UArrowComponent* Arrow;
+
+
+	// TODO: Move to component later??
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float DefaultMovementSpeed;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MovementSpeedModifier;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float PanSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "State")
+	bool IsAlive;
+	UPROPERTY(EditAnywhere, Category = "State")
+	bool IsBoosting;
+
+	UPROPERTY(EditAnywhere, Category = "VectorsRotators")
+	FVector PreviousPosition;
+	UPROPERTY(EditAnywhere, Category = "VectorsRotators")
+	FVector PreviousForwardVector;
+	UPROPERTY(EditAnywhere, Category = "VectorsRotators")
+	FRotator RelativeRotation;
+
+	UPROPERTY(EditAnywhere, Category = "Mechanics")
+	float Fuel;
+	UPROPERTY(EditAnywhere, Category = "Mechanics")
+	float FuelBurnRate;
+
+public:
+	// Sets default values for this character's properties
+	ABike();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	/** Called for turn input */
+	void Turn(const FInputActionValue& Value);
+	/** Called for pivot input */
+	void Pivot(const FInputActionValue& Value);
+
+	void ConstantForwardMovement();
+	void StartBoosting();
+	void StopBoosting();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
