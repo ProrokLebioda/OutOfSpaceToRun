3f61dc261b908dd95773e47e4657bb5b296682a1
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-08-03 10:45:33 +0200

    For some reason: Moving parts of UI code to C++ (Maybe to later move other parts to C++)

---
+++ Source/OutOfSpaceToRun/UI/HUD/UI_Minimap.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OutOfSpaceToRun/UI/HUD/UI_Minimap.h"
+#include "Components/Image.h"
+#include "UI_Minimap.h"
+
+void UUI_Minimap::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+FSlateBrush UUI_Minimap::GetMinimapImageBrush()
+{
+	return BrushMinimapMaterial;
+}
+
+void UUI_Minimap::SetMinimapImageBrush(FSlateBrush NewSlateBrush)
+{
+	BrushMinimapMaterial = NewSlateBrush;
+}
---
+++ Source/OutOfSpaceToRun/UI/HUD/UI_Minimap.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Styling/SlateBrush.h"
+#include "UI_Minimap.generated.h"
+
+class UImage;
+
+/**
+ * 
+ */
+UCLASS()
+class OUTOFSPACETORUN_API UUI_Minimap : public UUserWidget
+{
+	GENERATED_BODY()
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	// TODO: Had to uncomment in build file "Slate", perhaps there is a better way to paint Minimap, take a look later
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = true))
+	FSlateBrush BrushMinimapMaterial;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UImage* MinimapImage;
+
+public:
+	UFUNCTION(BlueprintGetter)
+	FSlateBrush GetMinimapImageBrush();
+
+	UFUNCTION(BlueprintSetter)
+	void SetMinimapImageBrush(FSlateBrush NewSlateBrush);
+};
